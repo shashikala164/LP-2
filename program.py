26a56bc54f12e3f568539a0e24292406ed1cd22b
shashikala164 <120574652+shashikala164@users.noreply.github.com>
2023-05-24 14:02:30 +0530

    program.py

---
+++ program.py
@@ -0,0 +1,31 @@
+def binary_search(arr,target):
+  low=0
+  high=len(arr)-1
+  
+  while(low<=high):
+    mid=(left+right)//2
+    
+    if arr[mid]==target:
+      return mid
+    
+    elif arr[mid]<target:
+      low=mid+1
+      
+      else:`
+        high=mid-1
+        
+        return -1
+      
+      #example
+      arr=input("enter the sorted elements").split()
+      arr=[int(num) for num in arr
+           
+           target=int(input("enter the sorted element"))
+           result= binary_search(arr,target)
+       
+           
+           if result != -1:
+                print("element are the found")
+           else:
+                print("not found")
+           
