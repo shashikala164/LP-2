6324583ced29d4b6d975c503d3cf465c2882dafb
shashikala164 <120574652+shashikala164@users.noreply.github.com>
2023-05-24 14:04:47 +0530

    program.py

--- program.py
+++ program.py
@@ -11,21 +11,22 @@ def binary_search(arr,target):
     elif arr[mid]<target:
       low=mid+1
       
-      else:`
-        high=mid-1
+     else:`
+      
+       high=mid-1
         
-        return -1
+       return -1
       
       #example
       arr=input("enter the sorted elements").split()
-      arr=[int(num) for num in arr
-           
-           target=int(input("enter the sorted element"))
-           result= binary_search(arr,target)
-       
-           
-           if result != -1:
-                print("element are the found")
-           else:
+      arr=[int(num) for num in arr]
+      target=int(input("enter the sorted element"))
+      result= binary_search(arr,target)
+      
+      if result != -1:
+        
+        print("element are the found")
+      else:
+        
                 print("not found")
            
